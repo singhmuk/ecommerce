6ba6d33cc7840fb5d1d89a8fe79347032c1287e7
mukesh <mukese0794@gmail.com>
2022-10-27 17:31:01 +0530

    password without hash

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const productSchems = require('../modules/user')
+const userSchema = require('../modules/user')
 
-router.route("/").post(productSchems)
-router.route("/").get(productSchems)
+router.get('/', async (req,res) => {
+    const userList = await userSchema.find();
+    if(!userList){
+        res.status(500).json({success: false})
+    }
+    res.send(userList);
+})
 
+router.post('/', async (req,res) => {
+    let user = userSchema({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+        return res.status(400).send('The user cannot be created')
+    res.send(user);
+})
 
 module.exports = router;
